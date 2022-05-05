6662f374a1c1b4a85cc61f15eeb95a734440ec80
Gabriel Bezerra <gabrieleliasmega@gmail.com>
2022-05-04 21:27:16 -0300

    algoritmo utilizando interface, finalizado!

---
+++ Entities/Invoice.cs
@@ -0,0 +1,27 @@
+using System.Globalization;
+
+namespace Entities
+{
+    public class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+        public Invoice(){}
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+       public double TotalPayment
+       {
+            get { return BasicPayment + Tax; }
+        }
+        public override string ToString()
+        {
+            return "Basic Payment" + BasicPayment.ToString("F2", CultureInfo.InvariantCulture)
+            + "\nTax: " + Tax.ToString("F2", CultureInfo.InvariantCulture) +
+            "\nTotalPayment: " + TotalPayment.ToString("F2", CultureInfo.InvariantCulture); 
+
+        }
+    }
+}
